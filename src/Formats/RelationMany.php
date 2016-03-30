71694d3efd80c77d4651801b3b3c3d05e2887e3a
oscarotero <oom@oscarotero.com>
2016-03-30 23:03:13 +0200

    new RelationMany format

---
+++ src/Formats/RelationMany.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Folk\Formats;
+
+use FormManager\Fields;
+use FormManager\Elements;
+use FormManager\Builder;
+use Folk\Entities\EntityInterface;
+
+class RelationMany extends RelationOne
+{
+    public function __construct(Builder $builder, EntityInterface $related)
+    {
+        parent::__construct($builder, $related);
+
+        $this->input->multiple();
+    }
+}
