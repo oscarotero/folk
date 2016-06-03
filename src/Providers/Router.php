be542c35e816370312c5101ad6913d1324392472
oscarotero <oom@oscarotero.com>
2016-06-03 17:17:41 +0200

    removed empty lines

--- src/Providers/Router.php
+++ src/Providers/Router.php
@@ -17,11 +17,8 @@ class Router implements ServiceProviderInterface
             $ns = 'Folk\\Controllers';
 
             $map->get('index', '/', "{$ns}\\Index");
-
             $map->get('search', '/{entity}', "{$ns}\\SearchEntity");
-
             $map->get('insert', '/{entity}/new', "{$ns}\\InsertEntity");
-
             $map->put('create', '/{entity}', "{$ns}\\CreateEntity");
             $map->get('read', '/{entity}/{id}', "{$ns}\\ReadEntity");
             $map->post('update', '/{entity}/{id}', "{$ns}\\UpdateEntity");
