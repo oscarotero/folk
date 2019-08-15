b41b74c8b4c15b1f38315a11fa425090673ab664
oscarotero <oom@oscarotero.com>
2019-08-15 18:22:19 +0200

    updated deps

--- demo/index.php
+++ demo/index.php
@@ -2,13 +2,21 @@
 
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Uri;
-use Zend\Diactoros\Response\SapiStreamEmitter;
+use Middlewares\Emitter;
+use Middleland\Dispatcher;
 
 include dirname(__DIR__).'/vendor/autoload.php';
 
 //php -S localhost:8888 demo/index.php
-$app = new Demo\Admin(__DIR__, new Uri('http://localhost:8888'));
 
-$response = $app->handle(ServerRequestFactory::fromGlobals());
 
-(new SapiStreamEmitter())->emit($response);
+$dispatcher = new Dispatcher([
+    new Emitter(),
+
+    function ($request) {
+        $admin = new Demo\Admin(__DIR__, new Uri('http://localhost:8888'));
+        return $admin->handle($request);
+    }
+]);
+
+$dispatcher->dispatch(ServerRequestFactory::fromGlobals());
